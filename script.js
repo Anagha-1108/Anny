8cc1ce67d99b7c37c12f271c1ce467070b0dffed
Anagha-1108 <anaghaml1108@gmail.com>
2025-01-30 13:34:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example script that could validate form input
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents default submission behavior
+     
+   alert("Your message has been sent!"); // Placeholder alert message.
+   });
+   
